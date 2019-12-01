deeb13b79e156e3351ccab428a1afb2e3069fab8
JUNG YOUNGBEEN <kyuzizimine@naver.com>
2019-12-01 21:53:23 +0900

    delect previous contact css

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,42 +1,75 @@
-// Input Lock
-$('textarea').blur(function () {
-  $('#hire textarea').each(function () {
-      $this = $(this);
-      if ( this.value != '' ) {
-        $this.addClass('focused');
-        $('textarea + label + span').css({'opacity': 1});
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
       }
-      else {
-        $this.removeClass('focused');
-        $('textarea + label + span').css({'opacity': 0});
-      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
   });
 });
 
-$('#hire .field:first-child input').blur(function () {
-  $('#hire .field:first-child input').each(function () {
-      $this = $(this);
-      if ( this.value != '' ) {
-        $this.addClass('focused');
-        $('.field:first-child input + label + span').css({'opacity': 1});
-      }
-      else {
-        $this.removeClass('focused');
-        $('.field:first-child input + label + span').css({'opacity': 0});
-      }
-  });
-});
-
-$('#hire .field:nth-child(2) input').blur(function () {
-  $('#hire .field:nth-child(2) input').each(function () {
-      $this = $(this);
-      if ( this.value != '' ) {
-        $this.addClass('focused');
-        $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-      }
-      else {
-        $this.removeClass('focused');
-        $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-      }
-  });
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
 });
